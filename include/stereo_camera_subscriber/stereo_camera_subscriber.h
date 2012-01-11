5bef7cc44c52a6daa4e46458a019b4804b40008c
volker <vnannen@gmail.com>
2012-01-11 15:42:00 +0100

    uncrustify

--- include/stereo_camera_subscriber/stereo_camera_subscriber.h
+++ include/stereo_camera_subscriber/stereo_camera_subscriber.h
@@ -31,73 +31,79 @@ public:
 
   static StereoCameraSubscriber
   subscribeStereoCamera( image_transport::ImageTransport & image_it,
-                                ros::NodeHandle & info_nh,
-                                const std::string & base_topic_left,
-                                const std::string & base_topic_right,
-                                uint32_t queue_size,
-                                const Callback& callback,
-                                const ros::VoidPtr & tracked_object = ros::VoidPtr(), 
-                                const image_transport::TransportHints &
-				transport_hints = image_transport::TransportHints() ) {
-    return StereoCameraSubscriber(image_it, info_nh, base_topic_left, base_topic_right, 
-                                  queue_size, callback, tracked_object, transport_hints);
-  }
-
-  static StereoCameraSubscriber
-  subscribeStereoCamera( image_transport::ImageTransport & image_it,
-                                ros::NodeHandle & info_nh,
-                                const std::string & base_topic_left,
-                                const std::string & base_topic_right,
+                         ros::NodeHandle & info_nh,
+                         const std::string & base_topic_left,
+                         const std::string & base_topic_right,
                          uint32_t queue_size,
-                                void(*fp)(const sensor_msgs::ImageConstPtr &,
-                                          const sensor_msgs::CameraInfoConstPtr &,
-                                          const sensor_msgs::ImageConstPtr &,
-                                          const sensor_msgs::CameraInfoConstPtr & ),
-                               const image_transport::TransportHints &
-			 transport_hints = image_transport::TransportHints() ) {
-    return subscribeStereoCamera( image_it, info_nh, 
-                                  base_topic_left, base_topic_right, queue_size,
-                                  Callback(fp), ros::VoidPtr(), transport_hints );
+                         const Callback & callback,
+                         const ros::VoidPtr & tracked_object = ros::VoidPtr(),
+                         const image_transport::TransportHints &
+                         transport_hints = image_transport::TransportHints() ) {
+    return StereoCameraSubscriber( image_it, info_nh,
+                                   base_topic_left, base_topic_right,
+                                   queue_size,
+                                   callback, tracked_object, transport_hints );
   }
 
-  template<class T>
   static StereoCameraSubscriber
   subscribeStereoCamera( image_transport::ImageTransport & image_it,
-                                ros::NodeHandle & info_nh,
-                                const std::string & base_topic_left,
-                                const std::string & base_topic_right,
-                                uint32_t queue_size,
-                                void(T::*fp)(const sensor_msgs::ImageConstPtr &,
-                                             const sensor_msgs::CameraInfoConstPtr &,
-                                             const sensor_msgs::ImageConstPtr &,
-                                             const sensor_msgs::CameraInfoConstPtr & ),
-                                T * obj,
-                                const image_transport::TransportHints &
-				transport_hints = image_transport::TransportHints() ){
-    return subscribeStereoCamera( image_it, info_nh, 
+                         ros::NodeHandle & info_nh,
+                         const std::string & base_topic_left,
+                         const std::string & base_topic_right,
+                         uint32_t queue_size,
+                         void (*fp)
+                         ( const sensor_msgs::ImageConstPtr &,
+                           const sensor_msgs::CameraInfoConstPtr &,
+                           const sensor_msgs::ImageConstPtr &,
+                           const sensor_msgs::CameraInfoConstPtr & ),
+                         const image_transport::TransportHints &
+                         transport_hints = image_transport::TransportHints() ) {
+    return subscribeStereoCamera( image_it, info_nh,
                                   base_topic_left, base_topic_right, queue_size,
-                                  boost::bind(fp, obj, _1, _2, _3, _4), ros::VoidPtr(), 
+                                  Callback( fp ), ros::VoidPtr(),
                                   transport_hints );
   }
 
   template<class T>
   static StereoCameraSubscriber
   subscribeStereoCamera( image_transport::ImageTransport & image_it,
-                                ros::NodeHandle & info_nh,
-                                const std::string & base_topic_left,
-                                const std::string & base_topic_right,
-                                uint32_t queue_size,
-                                void(T::*fp)(const sensor_msgs::ImageConstPtr &,
-                                             const sensor_msgs::CameraInfoConstPtr &,
-                                             const sensor_msgs::ImageConstPtr &,
-                                             const sensor_msgs::CameraInfoConstPtr & ),
-                                const boost::shared_ptr<T>& obj,
-                                const image_transport::TransportHints &
-				transport_hints = image_transport::TransportHints() ){
-    return subscribeStereoCamera( image_it, info_nh, 
+                         ros::NodeHandle & info_nh,
+                         const std::string & base_topic_left,
+                         const std::string & base_topic_right,
+                         uint32_t queue_size,
+                         void (T::* fp)
+                         ( const sensor_msgs::ImageConstPtr &,
+                           const sensor_msgs::CameraInfoConstPtr &,
+                           const sensor_msgs::ImageConstPtr &,
+                           const sensor_msgs::CameraInfoConstPtr & ),
+                         T * obj,
+                         const image_transport::TransportHints &
+                         transport_hints = image_transport::TransportHints() ) {
+    return subscribeStereoCamera( image_it, info_nh,
                                   base_topic_left, base_topic_right, queue_size,
-                                  boost::bind(fp, obj.get(), _1, _2, _3, _4), obj, 
-                                  transport_hints );
+                                  boost::bind( fp, obj, _1, _2, _3, _4 ),
+                                  ros::VoidPtr(), transport_hints );
+  }
+
+  template<class T>
+  static StereoCameraSubscriber
+  subscribeStereoCamera( image_transport::ImageTransport & image_it,
+                         ros::NodeHandle & info_nh,
+                         const std::string & base_topic_left,
+                         const std::string & base_topic_right,
+                         uint32_t queue_size,
+                         void (T::* fp)
+                         ( const sensor_msgs::ImageConstPtr &,
+                           const sensor_msgs::CameraInfoConstPtr &,
+                           const sensor_msgs::ImageConstPtr &,
+                           const sensor_msgs::CameraInfoConstPtr & ),
+                         const boost::shared_ptr<T> & obj,
+                         const image_transport::TransportHints &
+                         transport_hints = image_transport::TransportHints() ) {
+    return subscribeStereoCamera( image_it, info_nh,
+                                  base_topic_left, base_topic_right, queue_size,
+                                  boost::bind( fp, obj.get(), _1, _2, _3, _4 ),
+                                  obj, transport_hints );
   }
 
   std::string getLeftTopic() const;
