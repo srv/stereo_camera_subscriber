2185f06cfec51b5a150197b770d43c2aeb9f1ed9
volker <vnannen@gmail.com>
2012-02-03 14:26:22 +0100

    roll

--- src/stereo_camera_subscriber.cpp
+++ src/stereo_camera_subscriber.cpp
@@ -172,11 +172,11 @@ StereoCameraSubscriber( image_transport::ImageTransport & image_it,
   // Complain every 10s if it appears that the image and info topics 
   // are not synchronized
   impl_->image_sub_left_.
-  registerCallback( report );
+  registerCallback( &report );
 //  impl_->info_sub_left_.
 //  registerCallback( boost::bind( report ) );
   impl_->image_sub_right_.
-  registerCallback( report );
+  registerCallback( &report );
 //  impl_->info_sub_right_.
 //  registerCallback( boost::bind( report ) );
     
