65dd9fda4217c77bd1251b199bc41d4e96467930
volker <vnannen@gmail.com>
2012-02-09 15:02:11 +0100

    roll

--- src/stereo_camera_subscriber.cpp
+++ src/stereo_camera_subscriber.cpp
@@ -182,14 +182,14 @@ StereoCameraSubscriber( image_transport::ImageTransport & image_it,
 
   // Complain every 10s if it appears that the image and info topics 
   // are not synchronized
-  impl_->image_sub_left_.registerCallback( boost::bind( image_report, _1, 
-                                                        "left " ) );
-  impl_->info_sub_left_.registerCallback( boost::bind( info_report, _1, 
-                                                       "left " ) );
-  impl_->image_sub_right_.registerCallback( boost::bind( image_report, _1, 
-                                                         "right" ) );
-  impl_->info_sub_right_.registerCallback( boost::bind( info_report, _1, 
-                                                        "right" ) );
+//  impl_->image_sub_left_.registerCallback( boost::bind( image_report, _1, 
+//                                                        "left " ) );
+//  impl_->info_sub_left_.registerCallback( boost::bind( info_report, _1, 
+//                                                       "left " ) );
+//  impl_->image_sub_right_.registerCallback( boost::bind( image_report, _1, 
+//                                                         "right" ) );
+//  impl_->info_sub_right_.registerCallback( boost::bind( info_report, _1, 
+//                                                        "right" ) );
     
   impl_->image_sub_left_.
   registerCallback( boost::bind( increment, &impl_->image_received_left_ ) );
